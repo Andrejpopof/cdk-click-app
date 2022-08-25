38c7263c3d60055e9a8e3b3c6e6b8fed8bdb8167
Andrej Popov <Andrej.Popov@netcetera.com>
2022-08-25 11:45:58 +0200

    Initial commit

---
+++ lib/dipl_project_v3_easy-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class DiplProjectV3EasyStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'DiplProjectV3EasyQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
