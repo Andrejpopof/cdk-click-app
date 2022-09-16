2312efac238608f1f61c3afdde62e11f8a458990
Andrej Popov <Andrej.Popov@netcetera.com>
2022-09-16 14:45:36 +0200

    Added Source and Build stages for the pipeline

--- bin/dipl_project_v3_easy.ts
+++ bin/dipl_project_v3_easy.ts
@@ -3,10 +3,15 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { DiplProjectV3EasyStack } from '../lib/dipl_project_v3_easy-stack';
 import { env } from 'process';
+import { PipelineStack } from '../lib/pipeline-stack';
 
 const app = new cdk.App();
 
 
 new DiplProjectV3EasyStack(app, 'DiplProjectV3EasyStack', {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: 'eu-central-1'}
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: 'us-east-1'}
 });
+
+new PipelineStack(app, 'Pipeline',{
+  env: {account: process.env.CDK_DEFAULT_ACCOUNT, region: 'us-east-1'}
+})
